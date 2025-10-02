e9dbdb2ff142fa0e75fb903be0a2e801a7a179cd
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2025-10-02 22:32:55 +0300

    Almost draft of necessary flows

--- hask/precision/src/Precision/Def.hs
+++ hask/precision/src/Precision/Def.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DeriveAnyClass  #-}
-{-# LANGUAGE QuasiQuotes     #-}
-{-# LANGUAGE RoleAnnotations #-}
-{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE RoleAnnotations      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE UndecidableInstances #-}
 -- |
 module Precision.Def where
 
@@ -15,7 +17,7 @@ import Data.Monoid.Statistics.Numeric
 import System.Random.MWC
 import System.Random.MWC.Distributions
 import System.FilePath ((</>))
-import GHC.Generics (Generic)
+import GHC.Generics (Generic,Generically(..))
 import Python.Inline
 import Python.Inline.QQ
 
@@ -26,6 +28,7 @@ import HDF5.HL.Serialize (H5Serialize(..))
 import OKA.Flow
 import OKA.Flow.Tools
 import OKA.Metadata.Encoding
+import OKA.Metadata
 import Precision.Flow
 
 
@@ -41,14 +44,24 @@ data Distribution
   deriving stock    (Show,Eq,Generic)
   deriving anyclass (FromJSON,ToJSON)
   deriving MetaEncoding via AsAeson Distribution
+  deriving (IsMeta,IsMetaPrim) via AsMeta '["prec","distrib"] Distribution
 
 data Estimator
   = EvalMeanNaive 
   | EvalMeanKBN
   deriving stock (Show,Read,Eq,Generic)
   deriving MetaEncoding via AsReadShow Estimator
+  deriving (IsMeta,IsMetaPrim) via AsMeta '["prec","est"] Estimator
 
+newtype NSamples = NSamples Int
+  deriving stock (Show,Read,Eq,Generic)
+  deriving newtype MetaEncoding
+  deriving (IsMeta,IsMetaPrim) via AsMeta '["prec","N"] NSamples
 
+newtype PRNG = PRNG [Word32]
+  deriving stock (Show,Read,Eq,Generic)
+  deriving newtype MetaEncoding
+  deriving (IsMeta,IsMetaPrim) via AsMeta '["prec","PRNG"] PRNG
 
 
 ----------------------------------------------------------------
@@ -83,11 +96,39 @@ initMpmath = do
      mp.mp.dps = 60
      |]
 
+data Res = Res
+  { est   :: !Double
+  , exact :: !Double
+  }
+  deriving stock (Show,Generic)
+  deriving H5.Element via Generically Res
+
+newtype Sample = Sample (VH.VecHDF5 Res)
+  deriving stock (Show)
+  deriving (FlowInput,FlowOutput,FlowArgument) via AsHDF5Encoded (VH.VecHDF5 Res)
+
+
+
+flowGenSample
+  :: () -> Flow' (Result Sample)
+flowGenSample = liftHaskellFun "flow-gen-sample" () go where
+  go :: (Distribution,Estimator,NSamples,PRNG) -> () -> IO Sample
+  go (distrib,est,NSamples n,PRNG key) () = pure $ Sample $
+    let xs = sample key n distrib
+    in undefined
+  
 
 ----------------------------------------------------------------
 -- Flow definition
 ----------------------------------------------------------------
 
+
+
+    
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
+    
 type role AsHDF5Encoded representational
 
 -- | Encoding of outputs using HDF5 files
